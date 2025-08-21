436ddb19d11a844f4b029a9203bb1a9945d11c82
grwaywee <koreawee@gmail.com>
2025-08-21 18:01:17 +0900

    🔧 Rename package to RNPrebuildforMac (align with repo identity)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let fm = FileManager.default
 let bins = (try? fm.contentsOfDirectory(atPath: "Binary"))?.filter{ $0.hasSuffix(".xcframework") } ?? []
 let targets: [Target] = bins.map { .binaryTarget(name: $0.replacingOccurrences(of: ".xcframework", with: ""), path: "Binary/\($0)") }
 let package = Package(
-  name: "OwluRNMacBinaries",
+  name: "RNPrebuildforMac",
   platforms: [.macOS(.v13)],
   products: targets.map { .library(name: $0.name, targets: [$0.name]) },
   targets: targets + [.target(name: "Placeholder", path: "Sources/Placeholder")]
